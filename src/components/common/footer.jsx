f486094f858c334746eb00e65db1bf621376970b
Ahmad Fardous Azimi <ahmadazimi@Ahmads-MacBook-Air.local>
2023-07-20 16:46:49 +0300

    Created the footer and header

---
+++ src/components/common/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <h2 className="font-pacifico absolute bottom-5">
+      Made with 🧡 by{" "}
+      <a
+        href="https://linktr.ee/azimifardous"
+        target="_blank"
+        className="underline"
+      >
+        Mingo
+      </a>
+    </h2>
+  );
+};
+
+export default Footer;
---
+++ src/components/common/header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <h1 className="font-pacifico text-3xl text-customRed">your note app</h1>
+  );
+};
+
+export default Header;
