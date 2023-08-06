e29f1416d7b6e592a1c11167ab2bcc28d81969b1
Ahmad Fardous Azimi <ferdous.azimi7@gmail.com>
2023-08-06 20:07:30 +0300

    Deleted the context folder

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import Sidebar from "./sideBar";
-import UserContext from "./context/userContext";
-import useUser from "./hooks/useUser";
 
 const Dashboard = ({ children }) => {
-  // const user = useUser();
   return (
     <>
       <Sidebar />
